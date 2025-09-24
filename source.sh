a269a536a75f75c5615effd5f072fcb4c6f308ad
poscat <poscat0x04@outlook.com>
2025-09-24 03:50:19 +0300

    fix file path

--- source.sh
+++ source.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
+ROOT=$(pwd)
+
 UPSTREAMKERNELVER=$(curl -s https://api.github.com/repos/microsoft/WSL2-Linux-Kernel/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 test -d $KERNELDIR/.git || git clone --branch $UPSTREAMKERNELVER --single-branch --depth 1 https://github.com/microsoft/WSL2-Linux-Kernel.git $KERNELDIR
 
@@ -14,8 +16,8 @@ test -d $ZFSDIR/.git || git clone --branch $UPSTREAMZFSVER --depth 1 https://git
 
 pushd $ZFSDIR
 ./autogen.sh
-./configure --enable-linux-builtin --with-linux=$KERNELDIR --with-linux-obj=$KERNELDIR
-./copy-builtin $KERNELDIR
+./configure --enable-linux-builtin --with-linux=$ROOT/$KERNELDIR --with-linux-obj=$ROOT/$KERNELDIR
+./copy-builtin $ROOT/$KERNELDIR
 popd
 
 echo "CONFIG_ZFS=m" >> $KERNELDIR/$KCONFIG_CONFIG
